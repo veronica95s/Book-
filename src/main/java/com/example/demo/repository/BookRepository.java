37640ce0b81321d84976100ebafd66f0eadf3584
veronica95s <veronica.wq83@gmail.com>
2025-11-19 10:14:19 -0300

    '1'

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -6,13 +6,10 @@ import com.example.demo.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    // Buscar livros por título (contém, ignorando maiúsc/minúsc)
     List<Book> findByTituloContainingIgnoreCase(String titulo);
 
-    // Buscar livros por nome do autor (contém, ignorando maiúsc/minúsc)
     List<Book> findByAutor_NomeContainingIgnoreCase(String nome);
 
-    // Buscar por título + autor (ambos opcionais)
     List<Book> findByTituloContainingIgnoreCaseAndAutor_NomeContainingIgnoreCase(
             String titulo,
             String nomeAutor
